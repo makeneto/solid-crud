e7fbefe99adadbb634feae43b0b05a4af65a4568
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-02-17 20:04:49 +0100

    Added framer motion on cart items

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,5 @@
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion"
+import { useState } from "react"
 
 export default function CartItem({ cartObj, setCart }) {
   const {
@@ -13,13 +13,13 @@ export default function CartItem({ cartObj, setCart }) {
     oldPrice,
     rating,
     numRatings,
-  } = cartObj;
+  } = cartObj
 
-  const [isRemoving, setIsRemoving] = useState(false);
+  const [isRemoving, setIsRemoving] = useState(false)
 
   const removeFromCart = (e, id) => {
-    e.preventDefault();
-    setIsRemoving(true);
+    e.preventDefault()
+    setIsRemoving(true)
     setTimeout(() => {
       setCart((prevCart) => prevCart.filter((jogo) => jogo.id !== id))
     }, 300)
@@ -82,5 +82,5 @@ export default function CartItem({ cartObj, setCart }) {
         </motion.a>
       )}
     </AnimatePresence>
-  );
+  )
 }
